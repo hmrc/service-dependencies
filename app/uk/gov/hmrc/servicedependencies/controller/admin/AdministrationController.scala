202ffcc74e82ff03d8e52cc268b418ebcb4ada6b
hughfdjackson <hughfdjackson@googlemail.com>
2018-04-30 15:42:31 +0100

    INFRA-5071: switch to using QueryStringBinders

--- app/uk/gov/hmrc/servicedependencies/controller/admin/AdministrationController.scala
+++ app/uk/gov/hmrc/servicedependencies/controller/admin/AdministrationController.scala
@@ -28,9 +28,9 @@ import uk.gov.hmrc.servicedependencies.service.DependencyDataUpdatingService
 class AdministrationController @Inject()(dependencyDataUpdatingService: DependencyDataUpdatingService)
     extends BaseController {
 
-  def reloadLibraryDependenciesForAllRepositories(force: Option[String] = None) = Action { implicit request =>
+  def reloadLibraryDependenciesForAllRepositories(force: Option[Boolean] = None) = Action { implicit request =>
     dependencyDataUpdatingService
-      .reloadCurrentDependenciesDataForAllRepositories(force = force.contains("true"))
+      .reloadCurrentDependenciesDataForAllRepositories(force = force.getOrElse(false))
       .map(_ => Logger.debug(s"""${">" * 10} done ${"<" * 10}"""))
       .onFailure {
         case ex => throw new RuntimeException("reload of dependencies failed", ex)
