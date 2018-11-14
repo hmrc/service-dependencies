efbbc238ae758b3760f16444980b8b4880a594c5
Chris Turner <christopherjturner@gmail.com>
2018-11-14 14:41:49 +0000

    BDOG-34 upgraded to play26, github-client 2.5

--- test/uk/gov/hmrc/servicedependencies/config/ServiceDependenciesConfigTest.scala
+++ test/uk/gov/hmrc/servicedependencies/config/ServiceDependenciesConfigTest.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.servicedependencies.config
 
 import org.mockito.ArgumentMatchers.any
@@ -18,8 +34,9 @@ class ServiceDependenciesConfigTest extends FunSpec with Matchers with MockitoSu
         "github.open.api.host" -> "https://api.test.url",
         "github.open.api.user" -> "testuser",
         "github.open.api.key"  -> "key123")
+
       val serviceConfig = mock[ServicesConfig]
-      when(serviceConfig.baseUrl(any()))
+      when(serviceConfig.baseUrl(any())).thenReturn("")
 
       val sdc = new ServiceDependenciesConfig(config, serviceConfig)
 
