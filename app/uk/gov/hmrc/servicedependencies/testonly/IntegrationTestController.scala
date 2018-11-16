c8e251655704fefa4bb9027a84caa4ba168ea84c
Chris Turner <christopherjturner@gmail.com>
2018-11-16 10:14:55 +0000

    BDOG-47 Added test controller and routes

---
+++ app/uk/gov/hmrc/servicedependencies/testonly/IntegrationTestController.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.servicedependencies.testonly
+import javax.inject.Inject
+import play.api.libs.json._
+import play.api.mvc.ControllerComponents
+import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
+import uk.gov.hmrc.play.bootstrap.controller.BackendController
+import uk.gov.hmrc.servicedependencies.model.{MongoLibraryVersion, MongoRepositoryDependencies, MongoSbtPluginVersion}
+import uk.gov.hmrc.servicedependencies.persistence.{LibraryVersionRepository, RepositoryLibraryDependenciesRepository, SbtPluginVersionRepository}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class IntegrationTestController @Inject() (libraryRepo:                LibraryVersionRepository,
+                                           sbtPluginVersionRepository: SbtPluginVersionRepository,
+                                           dependenciesRepository:     RepositoryLibraryDependenciesRepository,
+                                           cc: ControllerComponents) extends BackendController(cc) {
+
+
+
+  implicit val dtf                  = ReactiveMongoFormats.dateTimeFormats
+  implicit val libraryVersionFormat = Json.using[Json.WithDefaultValues].format[MongoLibraryVersion]
+  implicit val sbtVersionFormat     = Json.using[Json.WithDefaultValues].format[MongoSbtPluginVersion]
+  implicit val dependenciesFormat   = Json.using[Json.WithDefaultValues].format[MongoRepositoryDependencies]
+
+  private def validateJson[A : Reads] = parse.json.validate(
+    _.validate[A].asEither.left.map(e => BadRequest(JsError.toJson(e)))
+  )
+
+
+  def addLibraryVersion = Action.async(validateJson[Seq[MongoLibraryVersion]]) { implicit request =>
+    Future.sequence(request.body.map(libraryRepo.update)).map(_ => Ok("Done"))
+  }
+
+
+  def addSbtVersion = Action.async(validateJson[Seq[MongoSbtPluginVersion]]) { implicit request =>
+    Future.sequence(request.body.map(sbtPluginVersionRepository.update)).map(_ => Ok("Done"))
+  }
+
+
+  def addDependencies = Action.async(validateJson[Seq[MongoRepositoryDependencies]]) { implicit request =>
+    Future.sequence(request.body.map(dependenciesRepository.update)).map(_ => Ok("Done"))
+  }
+
+}
