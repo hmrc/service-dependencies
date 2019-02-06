c53c220af90d4d433a234c418fbd1ed6b12778fc
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2019-02-06 10:50:52 +0000

    bump mongo version to 0.16.1

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ private object AppDependencies {
     "uk.gov.hmrc"       %% "github-client"        % "2.5.0",
     "uk.gov.hmrc"       %% "metrix"               % "3.2.0-play-26",
     "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.0.0-play-26",
-    "uk.gov.hmrc"       %% "mongo-lock"           % "6.1.0-play-26",
+    "uk.gov.hmrc"       %% "mongo-lock"           % "6.6.0-play-26",
     "com.typesafe.play" %% "play-json-joda"       % "2.6.0",
     "org.typelevel"     %% "cats"                 % "0.9.0",
     "org.apache.commons" % "commons-compress"     % "1.18"
@@ -23,7 +23,7 @@ private object AppDependencies {
     "uk.gov.hmrc"            %% "hmrctest"           % "3.2.0"              % Test,
     "org.scalatest"          %% "scalatest"          % "3.0.4"              % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"              % Test,
-    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.1.0-play-26"      % Test,
+    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.6.0-play-26"      % Test,
     "org.mockito"            %  "mockito-core"       % "2.2.6"              % Test,
     "org.pegdown"            %  "pegdown"            % "1.6.0"              % Test,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current  % Test,
