7e20041172d9de7fb69aa64484c2a8daf811c1bb
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-04-08 10:50:32 +0100

    PBD-3139 Move population of derived group-artefacts into DerivedGroupArtefactRepository

--- app/uk/gov/hmrc/servicedependencies/persistence/derived/DerivedGroupArtefactRepository.scala
+++ app/uk/gov/hmrc/servicedependencies/persistence/derived/DerivedGroupArtefactRepository.scala
@@ -16,7 +16,13 @@
 
 package uk.gov.hmrc.servicedependencies.persistence.derived
 
-import com.google.inject.{Inject, Singleton}
+import javax.inject.{Inject, Singleton}
+import org.mongodb.scala.model.Accumulators._
+import org.mongodb.scala.model.Aggregates._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Indexes.ascending
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
 import uk.gov.hmrc.servicedependencies.model._
@@ -24,16 +30,48 @@ import uk.gov.hmrc.servicedependencies.model._
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class DerivedGroupArtefactRepository @Inject()(mongoComponent: MongoComponent)(implicit ec: ExecutionContext
-                                                            ) extends PlayMongoRepository[GroupArtefacts](
-    collectionName = DerivedMongoCollections.artefactLookup
-  , mongoComponent = mongoComponent
-  , domainFormat   = MongoSlugInfoFormats.groupArtefactsFormat
-  , indexes        = Seq()
-  , optSchema      = None
+class DerivedGroupArtefactRepository @Inject()(
+  mongoComponent: MongoComponent
+)(implicit
+  ec: ExecutionContext
+) extends PlayMongoRepository[GroupArtefacts](
+  collectionName = "DERIVED-artefact-lookup",
+  mongoComponent = mongoComponent,
+  domainFormat   = MongoSlugInfoFormats.groupArtefactsFormat,
+  indexes        = Seq.empty,
+  optSchema      = None
 ){
+  def populate(): Future[Unit] =
+    mongoComponent.database.getCollection("slugInfos")
+      .aggregate(
+        List(
+          // Pull out any slug infos that are present in any environment (or latest)
+          `match`(
+            or(
+              SlugInfoFlag.values.map(f => equal(f.asString, true)): _* // filter for reachable data
+            )
+          ),
+          // Pull out just the dependencies array
+          project(
+            fields(
+              computed("dependencies", "$dependencies")
+            )
+          ),
+          // Create a new document for each dependency
+          unwind("$dependencies"),
+          // Exclude slug internals
+          `match`(regex("dependencies.version", "^(?!.*-assets$)(?!.*-sans-externalized$).*$")),
+          // Group by artefact group id
+          group("$dependencies.group", addToSet("artifacts",  "$dependencies.artifact")),
+          sort(orderBy(ascending("_id"))),
+          // replace content of target collection
+          out("DERIVED-artefact-lookup")
+        )
+      )
+      .allowDiskUse(true)
+      .toFuture
+      .map(_ => Unit)
 
-  def findGroupsArtefacts: Future[Seq[GroupArtefacts]] = collection.find().toFuture()
-
+  def findGroupsArtefacts: Future[Seq[GroupArtefacts]] =
+    collection.find().toFuture()
 }
-
