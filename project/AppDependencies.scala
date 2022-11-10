d704e6a741146d08a3cc9384924406eb3eeb8527
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2022-11-10 10:21:13 +0000

    Update bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport.{ehcache, ws}
 
-  val bootstrapPlayVersion = "7.10.0"
+  val bootstrapPlayVersion = "7.11.0"
   val hmrcMongoVersion     = "0.73.0"
 
   val compile = Seq(
