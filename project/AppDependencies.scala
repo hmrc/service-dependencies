453d935d860203886970fc36b0e83f6d1685ba81
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2023-08-29 09:14:29 +0100

    BDOG-2761 Play 2.9

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,24 +4,24 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport.{ehcache, ws}
 
-  val bootstrapPlayVersion = "7.22.0"
-  val hmrcMongoVersion     = "1.3.0"
+  val bootstrapPlayVersion = "7.13.0-SNAPSHOT"
+  val hmrcMongoVersion     = "1.0.0-SNAPSHOT"
 
   val compile = Seq(
     ws,
     ehcache,
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-28"        % hmrcMongoVersion,
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-29" % bootstrapPlayVersion,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-29"        % hmrcMongoVersion,
     "org.typelevel"          %% "cats-core"                 % "2.10.0",
     "org.apache.commons"     %  "commons-compress"          % "1.20",
     "software.amazon.awssdk" %  "sqs"                       % "2.20.155",
-    "org.yaml"               %  "snakeyaml"                 % "1.33"
+    "org.yaml"               %  "snakeyaml"                 % "2.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapPlayVersion % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion     % Test,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.16.46"            % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-29"  % bootstrapPlayVersion % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-29" % hmrcMongoVersion     % Test,
+    "org.mockito"            %% "mockito-scala-scalatest" % "1.17.14"            % Test,
     "com.typesafe.akka"      %% "akka-testkit"            % PlayVersion.akkaVersion % Test
   )
 }
