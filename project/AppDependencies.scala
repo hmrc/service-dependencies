3c75cc04da4f35c44b5015b1940e17d69ffdc445
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-08-13 15:41:24 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,25 +2,24 @@ import sbt._
 
 private object AppDependencies {
   import play.core.PlayVersion
-  import play.sbt.PlayImport.{ehcache, ws}
+  import play.sbt.PlayImport.{caffeine, ws}
 
-  val bootstrapPlayVersion = "9.0.0"
-  val hmrcMongoVersion     = "2.0.0"
+  val bootstrapPlayVersion = "9.3.0"
+  val hmrcMongoVersion     = "2.2.0"
 
   val compile = Seq(
     ws,
-    ehcache,
+    caffeine,
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "org.typelevel"          %% "cats-core"                 % "2.10.0",
+    "org.typelevel"          %% "cats-core"                 % "2.12.0",
     "org.apache.commons"     %  "commons-compress"          % "1.20",
-    "software.amazon.awssdk" %  "sqs"                       % "2.20.155",
+    "software.amazon.awssdk" %  "sqs"                       % "2.27.3"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapPlayVersion % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongoVersion     % Test,
-    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"           % Test,
     "org.scalacheck"         %% "scalacheck"              % "1.17.0"             % Test
   )
 }
